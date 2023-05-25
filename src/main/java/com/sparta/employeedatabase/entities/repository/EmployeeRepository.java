4615fdf1df13d6ca9fd9af742c5793f67a85e07b
Eesa Mannan <emannan@spartaglobal.com>
2023-05-25 14:57:40 +0100

    EmployeeRepository and EmployeeService added

--- src/main/java/com/sparta/employeedatabase/entities/repository/EmployeeRepository.java
+++ src/main/java/com/sparta/employeedatabase/entities/repository/EmployeeRepository.java
@@ -1,7 +1,13 @@
 package com.sparta.employeedatabase.entities.repository;
 
 import com.sparta.employeedatabase.entities.dto.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByLastName(String lastName);
+
+
 }
