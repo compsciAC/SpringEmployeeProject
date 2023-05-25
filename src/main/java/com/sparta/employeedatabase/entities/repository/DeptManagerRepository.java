e22f0b1c51eecfdb046bbda963798106c1edd90a
Adnan Siddiqui <msiddiqui@spartaglobal.com>
2023-05-25 17:16:31 +0100

    Added find Manager in dept & year query in DeptManagerRepository

--- src/main/java/com/sparta/employeedatabase/entities/repository/DeptManagerRepository.java
+++ src/main/java/com/sparta/employeedatabase/entities/repository/DeptManagerRepository.java
@@ -3,6 +3,19 @@ package com.sparta.employeedatabase.entities.repository;
 import com.sparta.employeedatabase.entities.dto.DeptManager;
 import com.sparta.employeedatabase.entities.dto.DeptManagerId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DeptManagerRepository extends JpaRepository<DeptManager, DeptManagerId> {
+
+    @Query(value = "SELECT d.id, d.deptName,e.firstName,e.lastName\n" +
+        "FROM DeptManager dm, Department d, Employee e\n" +
+        "where dm.deptNo=d.id\n" +
+        "and e.id=dm.id.empNo\n" +
+        "and :yr >= year(dm.fromDate)\n" +
+        "and :yr <= year(dm.toDate)\n" +
+        "and d.id=:deptName")
+
+    List<Integer> findManagerForDeptInYear(String deptName, String yr);
 }
