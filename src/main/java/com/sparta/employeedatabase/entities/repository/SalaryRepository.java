65bab1779d914dc9f06d8f1f42a3695685d6978b
Adnan Siddiqui <msiddiqui@spartaglobal.com>
2023-05-25 12:14:43 +0100

    Added avg salary query in SalaryRepository

--- src/main/java/com/sparta/employeedatabase/entities/repository/SalaryRepository.java
+++ src/main/java/com/sparta/employeedatabase/entities/repository/SalaryRepository.java
@@ -3,6 +3,18 @@ package com.sparta.employeedatabase.entities.repository;
 import com.sparta.employeedatabase.entities.dto.Salary;
 import com.sparta.employeedatabase.entities.dto.SalaryId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SalaryRepository extends JpaRepository<Salary, SalaryId> {
+
+    @Query(value ="select avg(s.salary) \n" +
+            "from Salary s, DeptEmp de, Department d\n" +
+            "where s.empNo = de.empNo\n" +
+            "and de.deptNo=d.id\n" +
+            "and d.deptName=:deptName\n" +
+            "and :dt between de.fromDate and de.toDate\n" +
+            "and :dt between s.id.fromDate and s.toDate")
+        List<Integer> findDeptAvgSalaryInYear(String deptName, String dt);
 }
