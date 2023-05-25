8d2280719c0d0b2728d0f96dd3e253a9712dc765
shalamike <m.shalaby1994@gmail.com>
2023-05-25 17:15:22 +0100

    recreated salaryService class that was accidentally deleted

--- src/main/java/com/sparta/employeedatabase/service/SalaryService.java
+++ src/main/java/com/sparta/employeedatabase/service/SalaryService.java
@@ -1,2 +1,36 @@
-package com.sparta.employeedatabase.service;public class SalaryService {
+package com.sparta.employeedatabase.service;
+
+
+import com.sparta.employeedatabase.entities.repository.SalaryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalaryService {
+
+    private final SalaryRepository salaryRepository;
+
+    @Autowired
+    public SalaryService(SalaryRepository salaryRepository){
+        this.salaryRepository = salaryRepository;
+    }
+
+    public double avgSalaryByGender(String gender){
+        return salaryRepository.findAvgSalaryByGender(gender);
+    }
+
+    public String findGenderPercentageDifference(){
+        double avgMaleSalary = salaryRepository.findAvgSalaryByGender("M");
+        double avgFemaleSalary = salaryRepository.findAvgSalaryByGender("F");
+        if (avgMaleSalary >= avgFemaleSalary){
+            double percentageDiff = (avgMaleSalary - avgFemaleSalary)/avgMaleSalary * 100;
+            return "Male employees earn " +  percentageDiff + "% more then female employees";
+        } else{
+            double percentageDiff = (avgFemaleSalary - avgMaleSalary)/avgFemaleSalary * 100;
+            return "Female employees earn " + percentageDiff + "% more then male employees";
+        }
+
+
+    }
+
 }
