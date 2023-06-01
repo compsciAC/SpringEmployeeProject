5ab98972bda543b04c9fa4b865bdc933bde50b9e
shalamike <m.shalaby1994@gmail.com>
2023-06-01 14:00:26 +0100

    added service and controller classes

--- src/main/java/com/sparta/employeedatabase/controller/SalaryController.java
+++ src/main/java/com/sparta/employeedatabase/controller/SalaryController.java
@@ -1,6 +1,10 @@
 package com.sparta.employeedatabase.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.employeedatabase.entities.dto.Employee;
 import com.sparta.employeedatabase.entities.dto.Salary;
+import com.sparta.employeedatabase.entities.repository.EmployeeRepository;
 import com.sparta.employeedatabase.entities.repository.SalaryRepository;
 import com.sparta.employeedatabase.service.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,26 +13,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class SalaryController {
 
     private SalaryService salaryService;
+    private EmployeeRepository employeeRepository;
     private final SalaryRepository salaryRepository;
 
-    @Autowired SalaryController(SalaryService salaryService,
-                                SalaryRepository salaryRepository){
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    SalaryController(SalaryService salaryService, SalaryRepository salaryRepository, ObjectMapper objectMapper){
         this.salaryService = salaryService;
         this.salaryRepository = salaryRepository;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("salary/paygap")
     public ResponseEntity<String>getGenderPayGap(){
-        return getStringResponseEntity(salaryService.percentageDifferenceBetweenGenders(salaryService.avgSalaryByGender("M"), salaryService.avgSalaryByGender("F")));
+        return getStringResponseEntity(salaryService.findGenderPercentageDifference());
     }
 
     @GetMapping("salary/avgSalary/{gender}")
@@ -52,6 +68,88 @@ public class SalaryController {
                     httpHeaders, HttpStatus.OK);
         }
         return response;
+    }
+
+    @GetMapping(value = "/empsalary/{id}")
+    public ResponseEntity<String> getSalaryByEmpId(@PathVariable Integer id){
+        Optional<Employee>  employee = employeeRepository.findById(id);
+        Integer employeeSalary = salaryService.getEmployeeHighestSalaryByEmployeeId(id);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("content-type", "application/json");
+        if(employee.isPresent()){
+            ResponseEntity<String> employeePresentResponse = null;
+            employeePresentResponse = new ResponseEntity<>(
+                    "{"+ employee.get().getId() + ", " + employee.get().getFirstName() + " " + employee.get().getLastName()  + " : " + employeeSalary + "}",
+                    httpHeaders,
+                    HttpStatus.OK);
+            return employeePresentResponse;
+        } else {
+            return new ResponseEntity<>(
+                    "{\"message\" : \"That employee doesn't exist\"}",
+                    httpHeaders,
+                    HttpStatus.NOT_FOUND
+            );
+        }
+    }
+
+
+    @GetMapping(value = "/empabovsalary/{salary}")//this will return a list of employees
+    public ResponseEntity<String> getEmployeesEarningAboveSalary(@PathVariable Integer salary){
+        List<Employee> employees = salaryService.getEmployeeEarningAboveGivenSalary(salary);
+        ArrayList<String> employeesAndSalaries = new ArrayList<>();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("content-type", "application/json");
+        for (Employee employee: employees) {
+            employeesAndSalaries.add( employee.getId() +
+                    " " + employee.getFirstName() +
+                    " " + employee.getLastName() +
+                    " : " +
+                    salaryService.getEmployeeHighestSalaryByEmployeeId(employee.getId()));
+        }
+        if(employees.size()>0){
+            return new ResponseEntity<>(
+                    "{" + salary + ":" + employeesAndSalaries + "}",
+                    httpHeaders,
+                    HttpStatus.OK
+            );
+        }
+        return new ResponseEntity<>(
+                "{\"message\" : \"no Employees exist\"}",
+                httpHeaders,
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+
+    @PutMapping(value = "/salary/{empId}/{fromDate}/{toDate}")
+    ResponseEntity<String> salaryToUpdate(@PathVariable int empId,@PathVariable String fromDate,@PathVariable String toDate){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("content-type", "application/json");
+        try {
+            Date toDateAsDate = new SimpleDateFormat("yyyy-MM-dd").parse(toDate);
+            Date fromDateAsDate = new SimpleDateFormat("yyyy-MM-dd").parse(fromDate);
+            Optional<Salary> salaryToUpdate = salaryService.getSalaryByEmpIdAndFromDate(empId, fromDateAsDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+
+            if (salaryToUpdate.isPresent()){
+                salaryToUpdate.get().setToDate(toDateAsDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+                salaryService.saveSalary(salaryToUpdate.get());
+                ResponseEntity<String> salaryUpdatedResponse = new ResponseEntity<>(
+                        objectMapper.writeValueAsString(salaryToUpdate.get().toString()),
+                        httpHeaders,
+                        HttpStatus.OK
+                );
+                return salaryUpdatedResponse;
+            }else {
+                ResponseEntity<String> noSalaryToUpdate = new ResponseEntity<>(
+                        "{\"message\" : \"That salary doesn't exist to update\"}",
+                        httpHeaders,
+                        HttpStatus.NOT_FOUND);
+                return noSalaryToUpdate;
+            }
+        } catch (ParseException | JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
